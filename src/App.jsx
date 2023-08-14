7c918216fdcd649cc322009d79afaf102e35535d
chaunt <chaulea@gmail.com>
2023-08-14 15:20:32 +0700

    chroe: add animate cursor

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { Link, Navigate, Route, Routes } from 'react-router-dom';
 import { HistoryRouter as Router } from 'redux-first-history/rr6';
 import { useTranslation } from 'react-i18next';
+import AnimatedCursor from 'react-animated-cursor';
 
 import '@styles/App.scss';
 
@@ -16,6 +17,21 @@ function App() {
           <Route path='*' element={<Navigate to='/' />} />
         </Routes>
       </Router>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        showSystemCursor={true}
+        hasBlendMode={true}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)'
+        }}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+      />
     </Provider>
   );
 }
